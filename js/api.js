3f1ee643d5094346945dea6475dbd46341e2b245
hausop <49778900+hausop@users.noreply.github.com>
2026-07-29 15:38:07 +0800

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const API = "https://script.google.com/macros/s/AKfycbzevezXvC7qezFEXmrPl_uWY53R65Q2_vDdOu_GIIXqRDm_LV4A_zQByfjyY7SKNYu0/exec";
+const API = "/.netlify/functions/api";
 
 async function searchFiles(school, team) {
 
