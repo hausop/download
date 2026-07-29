865adc0ff6e234ff432c1efc4fce2bceda0d1106
hausop <49778900+hausop@users.noreply.github.com>
2026-07-29 14:36:25 +0800

    Create api.js

---
+++ js/api.js
@@ -0,0 +1,29 @@
+const API = "https://script.google.com/macros/s/AKfycbzevezXvC7qezFEXmrPl_uWY53R65Q2_vDdOu_GIIXqRDm_LV4A_zQByfjyY7SKNYu0/exec";
+
+async function searchFiles(school, team) {
+
+    const url =
+        `${API}?action=search&school=${encodeURIComponent(school)}&team=${encodeURIComponent(team)}`;
+
+    const response = await fetch(url);
+
+    if (!response.ok) {
+        throw new Error(`API Error: ${response.status}`);
+    }
+
+    return await response.json();
+}
+
+async function getGuide() {
+
+    const response =
+        await fetch(`${API}?action=guide`);
+
+    if (!response.ok) {
+        throw new Error(`API Error: ${response.status}`);
+    }
+
+    return await response.json();
+}
+
+console.log("api.js loaded");
