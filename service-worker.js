1d2048ab98c26ae54ed19382902560a0a6815cea
hausop <49778900+hausop@users.noreply.github.com>
2026-08-04 12:17:01 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,17 @@
+const CACHE_NAME = "certificate-downloader-v1";
 
+self.addEventListener("install", (event) => {
+
+    console.log("Service Worker 安裝完成");
+
+    self.skipWaiting();
+
+});
+
+self.addEventListener("activate", (event) => {
+
+    console.log("Service Worker 啟用");
+
+    event.waitUntil(clients.claim());
+
+});
